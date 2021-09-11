bf323f14e815819f19cc33b6083ee8344da33f71
Matthew Grove <me@mgrove.uk>
2021-09-11 18:14:18 +0100

    Account for moved css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect, Link } from 'react-router-dom';
 import Home from "./Home";
 import LoggedInHome from "./LoggedInHome";
