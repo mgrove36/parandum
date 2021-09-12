037cf046c040dc77b70731e3b232e8c51c9335b5
Matthew Grove <me@mgrove.uk>
2021-09-12 20:27:52 +0100

    Remove unused prop from component

--- src/RouteChangeTracker.js
+++ src/RouteChangeTracker.js
@@ -1,6 +1,6 @@
 import { withRouter } from "react-router-dom";
 
-export default withRouter(function RouteChangeTracker({ history, logEvent }) {
+export default withRouter(function RouteChangeTracker({ history }) {
 	history.listen((location, action) => {
 		if (location.pathname === "/login") {
 			document.body.style.overflow = "hidden";
